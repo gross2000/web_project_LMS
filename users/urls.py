e252e125350d214b86bd49b03e223afd69509443
cheandrey <skyandreymann@gmail.com>
2024-11-22 00:21:49 +0300

    DRF_1

---
+++ users/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from rest_framework.permissions import AllowAny
+from rest_framework.routers import SimpleRouter
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView, )
+
+from users.apps import UsersConfig
+from users.views import (
+    UserViewSet,
+    UserCreateAPIView,
+)
+
+app_name = UsersConfig.name
+
+router = SimpleRouter()
+router.register("", UserViewSet)
+
+urlpatterns = [
+    path("login/", TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(permission_classes=(AllowAny,)), name="token_refresh"),
+    path("register/", UserCreateAPIView.as_view(), name="register"),
+] + router.urls
